79cd5514b7eb7cb93026bbe7184f4fb7afd39345
thomasjulien5-create <thomas.julien5@gmail.com>
2026-09-16 14:52:35 +0200

    Refresh PWA cache for Crampon readings

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='ad-fontes-v6';
-const STATIC=['./','./index.html','./styles.css?v=6','./app.js?v=6','./meditations.js?v=6','./manifest.webmanifest','./icon.svg'];
+const CACHE='ad-fontes-v7';
+const STATIC=['./','./index.html','./styles.css?v=7','./app.js?v=7','./meditations.js?v=7','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
