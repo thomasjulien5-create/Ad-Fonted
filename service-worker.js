e3fab626e4b02f393c7f9e3ba031f505fd29be5f
thomasjulien5-create <thomas.julien5@gmail.com>
2026-09-16 09:29:51 +0200

    Add offline service worker

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE='ad-fontes-v1';
+const STATIC=['./','./index.html','./styles.css','./app.js','./meditations.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  const url=new URL(e.request.url);
+  if(url.origin==='https://categpt.chat'){
+    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));
+  } else {
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
+});
