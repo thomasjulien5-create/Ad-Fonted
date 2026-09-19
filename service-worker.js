db58a1579fe6e0107681cbb930b99c729ec1dae3
thomasjulien5-create <thomas.julien5@gmail.com>
2026-09-19 12:52:04 +0200

    Bump PWA cache to v30

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='ad-fontes-v29';
-const STATIC=['./','./index.html','./installer/','./installer/index.html','./styles.css?v=29','./app.js?v=29','./meditations.js?v=29','./assets/file_0000000000c082108ce827725e006074.png?v=29','./manifest.webmanifest','./icon.svg'];
+const CACHE='ad-fontes-v30';
+const STATIC=['./','./index.html','./installer/','./installer/index.html','./styles.css?v=30','./app.js?v=30','./meditations.js?v=30','./assets/file_0000000000c082108ce827725e006074.png?v=30','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
