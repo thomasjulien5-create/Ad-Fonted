390fbc1d660d22ad5a98cfec83078eb9d173b4a1
thomasjulien5-create <thomas.julien5@gmail.com>
2026-09-19 12:59:17 +0200

    Force PWA refresh for exact homepage v32

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='ad-fontes-v31';
-const STATIC=['./','./index.html','./installer/','./installer/index.html','./styles.css?v=31','./app.js?v=31','./meditations.js?v=31','./assets/file_0000000000c082108ce827725e006074.png?v=31','./assets/home-bible.webp?v=31','./assets/home-thomas.webp?v=31','./assets/home-about.webp?v=31','./manifest.webmanifest','./icon.svg'];
+const CACHE='ad-fontes-v32';
+const STATIC=['./','./index.html','./installer/','./installer/index.html','./styles.css?v=32','./app.js?v=32','./meditations.js?v=32','./assets/file_0000000000c082108ce827725e006074.png?v=32','./assets/home-bible.webp?v=32','./assets/home-thomas.webp?v=32','./assets/home-about.webp?v=32','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
