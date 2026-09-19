428f459a76dc5b122786961223678ddc8a1c6b35
thomasjulien5-create <thomas.julien5@gmail.com>
2026-09-19 12:16:22 +0200

    Bump PWA cache to v23

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ad-fontes-v22';
+const CACHE='ad-fontes-v23';
 const STATIC=['./','./index.html','./installer/','./installer/index.html','./styles.css?v=22','./app.js?v=22','./meditations.js?v=22','./assets/file_0000000000c082108ce827725e006074.png?v=22','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
