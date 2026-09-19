74a90b49703838428ac242474d9ab5f2bfdb6ff2
thomasjulien5-create <thomas.julien5@gmail.com>
2026-09-19 14:39:15 +0200

    v33: refresh PWA home assets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ad-fontes-v32';
+const CACHE='ad-fontes-v33';
 const STATIC=['./','./index.html','./installer/','./installer/index.html','./styles.css?v=32','./app.js?v=32','./meditations.js?v=32','./assets/file_0000000000c082108ce827725e006074.png?v=32','./assets/home-bible.webp?v=32','./assets/home-thomas.webp?v=32','./assets/home-about.webp?v=32','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
