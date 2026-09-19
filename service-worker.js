9737b96b9dc4df3ee1917e2ab1d0c3f71a4d82ea
thomasjulien5-create <thomas.julien5@gmail.com>
2026-09-19 12:56:07 +0200

    Cache exact homepage artwork in v31

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='ad-fontes-v30';
-const STATIC=['./','./index.html','./installer/','./installer/index.html','./styles.css?v=30','./app.js?v=30','./meditations.js?v=30','./assets/file_0000000000c082108ce827725e006074.png?v=30','./manifest.webmanifest','./icon.svg'];
+const CACHE='ad-fontes-v31';
+const STATIC=['./','./index.html','./installer/','./installer/index.html','./styles.css?v=31','./app.js?v=31','./meditations.js?v=31','./assets/file_0000000000c082108ce827725e006074.png?v=31','./assets/home-bible.webp?v=31','./assets/home-thomas.webp?v=31','./assets/home-about.webp?v=31','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
