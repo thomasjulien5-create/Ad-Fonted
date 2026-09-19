303cd5f8cc97ac3064b1ec49c229f86283061682
thomasjulien5-create <thomas.julien5@gmail.com>
2026-09-19 14:47:01 +0200

    v34: refresh approved homepage

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='ad-fontes-v33';
-const STATIC=['./','./index.html','./installer/','./installer/index.html','./styles.css?v=32','./app.js?v=32','./meditations.js?v=32','./assets/file_0000000000c082108ce827725e006074.png?v=32','./assets/home-bible.webp?v=32','./assets/home-thomas.webp?v=32','./assets/home-about.webp?v=32','./manifest.webmanifest','./icon.svg'];
+const CACHE='ad-fontes-v34';
+const STATIC=['./','./index.html','./installer/','./installer/index.html','./styles.css?v=34','./app.js?v=34','./meditations.js?v=34','./assets/file_0000000000c082108ce827725e006074.png?v=34','./assets/home-bible.webp?v=34','./assets/home-thomas.webp?v=34','./assets/home-about.webp?v=34','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
